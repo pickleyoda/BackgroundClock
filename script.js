5015555d643b2cbde6c58a5e78e99d21567eb87e
Pickle Yoda <72274796+pickleyoda@users.noreply.github.com>
2021-12-26 19:35:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 var hr, min, sec;
 var h, m, s, i;
 var f = true;
+var mode = false;
 
 window.onload = function() {
+  document.getElementById("time").style.transform = "scale(1)";
+  document.getElementById("time").addEventListener("mouseover", glow);
+  document.getElementById("time").addEventListener("mouseout", stopglow);
   hr = document.getElementById("hr");
   min = document.getElementById("min");
   sec = document.getElementById("sec");
@@ -12,6 +16,7 @@ window.onload = function() {
 
   i = setInterval(initialise, 1);
 
+  document.getElementById("hoverbutton").addEventListener("mouseover", toggleClock);
 }
 
 function initialise(){
@@ -24,6 +29,11 @@ function initialise(){
     currentTime = new Date();
     m = currentTime.getMinutes();
     h = currentTime.getHours();
+
+    if (s > 9)
+    sec.innerHTML = s;
+    else
+    sec.innerHTML = '0'+s;
 
     if (m > 9)
     min.innerHTML = m;
@@ -54,6 +64,9 @@ function counting() {
     else
     min.innerHTML = '0' + m;
 
+    if(mode)
+    h = h % 12;
+
     if (h > 9)
     hr.innerHTML = h;
     else
@@ -61,4 +74,30 @@ function counting() {
 
     f = false;
   }
+}
+
+function toggleClock() {
+  mode = !mode;
+  h = currentTime.getHours();
+      if(mode)
+      h = h % 12;
+
+      if (h > 9)
+      hr.innerHTML = h;
+      else
+      hr.innerHTML = '0' + h;
+}
+
+function glow(){
+  document.getElementById("time").addEventListener("mousemove", moveglow);
+}
+
+function stopglow(){
+  document.getElementById("time").removeEventListener("mousemove", moveglow);
+}
+
+function moveglow(e){
+  element = document.getElementById("time");
+  element.style.setProperty("--x", e.clientX);
+  element.style.setProperty("--y", e.clientY);
 }
